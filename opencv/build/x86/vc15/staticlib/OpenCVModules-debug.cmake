339cf9067d0be16f1cd0464df2a0fd02845f1c51
coolxv <coolxv@gmail.com>
2018-10-24 10:58:23 +0800

    base

---
+++ opencv/build/x86/vc15/staticlib/OpenCVModules-debug.cmake
@@ -0,0 +1,119 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "zlib" for configuration "Debug"
+set_property(TARGET zlib APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(zlib PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x86/vc15/staticlib/zlibd.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS zlib )
+list(APPEND _IMPORT_CHECK_FILES_FOR_zlib "${_IMPORT_PREFIX}/x86/vc15/staticlib/zlibd.lib" )
+
+# Import target "libjpeg-turbo" for configuration "Debug"
+set_property(TARGET libjpeg-turbo APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(libjpeg-turbo PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x86/vc15/staticlib/libjpeg-turbod.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS libjpeg-turbo )
+list(APPEND _IMPORT_CHECK_FILES_FOR_libjpeg-turbo "${_IMPORT_PREFIX}/x86/vc15/staticlib/libjpeg-turbod.lib" )
+
+# Import target "libtiff" for configuration "Debug"
+set_property(TARGET libtiff APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(libtiff PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C;CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x86/vc15/staticlib/libtiffd.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS libtiff )
+list(APPEND _IMPORT_CHECK_FILES_FOR_libtiff "${_IMPORT_PREFIX}/x86/vc15/staticlib/libtiffd.lib" )
+
+# Import target "libwebp" for configuration "Debug"
+set_property(TARGET libwebp APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(libwebp PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x86/vc15/staticlib/libwebpd.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS libwebp )
+list(APPEND _IMPORT_CHECK_FILES_FOR_libwebp "${_IMPORT_PREFIX}/x86/vc15/staticlib/libwebpd.lib" )
+
+# Import target "libjasper" for configuration "Debug"
+set_property(TARGET libjasper APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(libjasper PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x86/vc15/staticlib/libjasperd.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS libjasper )
+list(APPEND _IMPORT_CHECK_FILES_FOR_libjasper "${_IMPORT_PREFIX}/x86/vc15/staticlib/libjasperd.lib" )
+
+# Import target "libpng" for configuration "Debug"
+set_property(TARGET libpng APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(libpng PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x86/vc15/staticlib/libpngd.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS libpng )
+list(APPEND _IMPORT_CHECK_FILES_FOR_libpng "${_IMPORT_PREFIX}/x86/vc15/staticlib/libpngd.lib" )
+
+# Import target "IlmImf" for configuration "Debug"
+set_property(TARGET IlmImf APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(IlmImf PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x86/vc15/staticlib/IlmImfd.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS IlmImf )
+list(APPEND _IMPORT_CHECK_FILES_FOR_IlmImf "${_IMPORT_PREFIX}/x86/vc15/staticlib/IlmImfd.lib" )
+
+# Import target "ippiw" for configuration "Debug"
+set_property(TARGET ippiw APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(ippiw PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x86/vc15/staticlib/ippiwd.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ippiw )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ippiw "${_IMPORT_PREFIX}/x86/vc15/staticlib/ippiwd.lib" )
+
+# Import target "libprotobuf" for configuration "Debug"
+set_property(TARGET libprotobuf APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(libprotobuf PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x86/vc15/staticlib/libprotobufd.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS libprotobuf )
+list(APPEND _IMPORT_CHECK_FILES_FOR_libprotobuf "${_IMPORT_PREFIX}/x86/vc15/staticlib/libprotobufd.lib" )
+
+# Import target "ittnotify" for configuration "Debug"
+set_property(TARGET ittnotify APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(ittnotify PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x86/vc15/staticlib/ittnotifyd.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ittnotify )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ittnotify "${_IMPORT_PREFIX}/x86/vc15/staticlib/ittnotifyd.lib" )
+
+# Import target "opencv_world" for configuration "Debug"
+set_property(TARGET opencv_world APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_world PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x86/vc15/staticlib/opencv_world343d.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_world )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_world "${_IMPORT_PREFIX}/x86/vc15/staticlib/opencv_world343d.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
